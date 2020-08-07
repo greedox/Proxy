40f50ac2b91730b8db70306043a6ba5de48e15c8
Igor Strunin <struninigor@mail.ru>
2020-08-07 21:14:32 +0300

    Parsers implementation with background service

---
+++ Proxy/Models/ParsedProxy.cs
@@ -0,0 +1,14 @@
+﻿namespace Proxy.Models
+{
+    public class ParsedProxy
+    {
+        public string Host { get; }
+        public int Port { get; }
+
+        public ParsedProxy(string host, int port)
+        {
+            Host = host;
+            Port = port;
+        }
+    }
+}
---
+++ Proxy/Services/ProxyParsers/FreeProxyListParser.cs
@@ -0,0 +1,41 @@
+﻿using Proxy.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Proxy.Services.ProxyParsers
+{
+    public class FreeProxyListParser : IProxyParser
+    {
+        private readonly HttpClient _http;
+
+        public FreeProxyListParser(IHttpClientFactory httpClientFactory)
+        {
+            _http = httpClientFactory.CreateClient();
+            _http.DefaultRequestHeaders.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0;) Gecko/20100101 Firefox/77.0");
+        }
+
+        public async IAsyncEnumerable<string> GetPagesForParse()
+        {
+            yield return "https://getfreeproxylists.blogspot.com/";
+        }
+
+        public async Task<ParsedProxy[]> ParsePage(string url)
+        {
+            var response = await _http.GetAsync(url);
+            var body = await response.Content.ReadAsStringAsync();
+
+            return Regex.Matches(body, @"(\d{2,3}\.\d{2,3}\.\d{2,3}\.\d{2,3}):(\d{2,5})")
+                .Select(s =>
+                {
+                    var host = s.Groups[1].Value;
+                    var port = int.Parse(s.Groups[2].Value);
+
+                    return new ParsedProxy(host, port);
+                })
+                .ToArray();
+        }
+    }
+}
